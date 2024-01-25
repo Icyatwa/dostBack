e232948e4593e8eebd8d98ce95bf273dfcc28672
Icyatwa <icyatwandoba@gmail.com>
2024-01-25 10:20:43 -1100

    index.js

--- index.js
+++ index.js
@@ -11,19 +11,13 @@ const app = express()
 
 // middleware
 app.use(express.json())
-app.use(cors({
-  origin: "https://dostfront.onrender.com", // Replace with your actual Render app domain
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  credentials: true,
-  optionsSuccessStatus: 204,
-}));
+app.use(cors());
 
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); 
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use((req, res, next) => {
+  console.log(req.path, req.method)
+  next()
+})
 
 // routes
 app.use('/api/ride', rideRoutes)
